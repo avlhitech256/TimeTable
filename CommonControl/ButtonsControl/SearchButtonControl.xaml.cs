d402b04702419af2f5e2aed1b57a6f45193779b0
avlhitech256 <avlhitech256@gmail.com>
2017-04-22 01:25:08 +0300

    Начата последняя фаза изменений для унифицирования Model и ViewModel. Начат вынос в отдельный проект мконтрола SearchButtonControl.cs, т. к. данный контрол будет использоватся всеми View контролами.

---
+++ CommonControl/ButtonsControl/SearchButtonControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommonControl.ButtonsControl
+{
+    /// <summary>
+    /// Логика взаимодействия для SearchButtonControl.xaml
+    /// </summary>
+    public partial class SearchButtonControl : UserControl
+    {
+        public SearchButtonControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
