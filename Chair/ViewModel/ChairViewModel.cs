e79df916d6e358641460e2f8c3466adc60bc3cf1
avlhitech256 <avlhitech256@gmail.com>
2017-05-02 22:20:39 +0300

    Завершено создание ChairViewModel

---
+++ Chair/ViewModel/ChairViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Domain.ViewModel;
+using System.Collections.ObjectModel;
+using Chair.Model;
+using DataService.Model;
+using Domain.DomainContext;
+
+namespace Chair.ViewModel
+{
+    public class ChairViewModel : ViewModel<DataService.Model.Chair>, IChairViewModel
+    {
+        #region Constructors
+
+        public ChairViewModel(IDomainContext context) : base(context, new ChairModel(context)) { }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Faculty> Faculties => (Model as IChairModel)?.Faculties;
+
+        public ObservableCollection<Faculty> FacultiesForSearch => (Model as IChairModel)?.FacultiesForSearch;
+
+        public ObservableCollection<Specialization> Specializations => (Model as IChairModel)?.Specializations;
+
+        #endregion
+    }
+
+}
---
+++ Chair/ViewModel/IChairViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using DataService.Model;
+using Domain.ViewModel;
+
+namespace Chair.ViewModel
+{
+    public interface IChairViewModel : IDataViewModel<DataService.Model.Chair>
+    {
+        ObservableCollection<Faculty> Faculties { get; }
+        ObservableCollection<Faculty> FacultiesForSearch { get; }
+        ObservableCollection<Specialization> Specializations { get; }
+    }
+
+}
